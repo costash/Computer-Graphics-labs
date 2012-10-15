f74cdb1cc1cb6d197fdea3d52f6e975bf581c8f7
Constantin Șerban-Rădoi <costashsrc@gmail.com>
2012-10-15 10:53:43 +0300

    Added lab2

---
+++ lab2/lab2/Transform3d.cpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "Transform3d.h"
+#include "Support3d.h"
+
+void Transform3d::translate(Point3d *pct, float tx, float ty, float tz){
+}
+
+void Transform3d::rotateX(Point3d *pct, float angleInRadians){
+	
+}
+void Transform3d::rotateY(Point3d *pct, float angleInRadians){
+	
+}
+void Transform3d::rotateZ(Point3d *pct, float angleInRadians){
+	
+}
+
+void Transform3d::rotateXRelativeToAnotherPoint(Point3d *pct, Point3d *ref, float angleInRadians){
+	
+}
+void Transform3d::rotateYRelativeToAnotherPoint(Point3d *pct, Point3d *ref, float angleInRadians){
+	
+}
+void Transform3d::rotateZRelativeToAnotherPoint(Point3d *pct, Point3d *ref, float angleInRadians){
+	
+}
+void Transform3d::scale(Point3d *pct, float sx, float sy, float sz){
+	
+}
+void Transform3d::scaleRelativeToAnotherPoint(Point3d *pct, Point3d *ref, float sx, float sy, float sz){
+	
+}
---
+++ lab2/lab2/main.cpp
@@ -0,0 +1,136 @@
+//-----------------------------------------------------------------------------------------------
+//					LAB 2
+//
+//	Fisiere de interes: Transform3d.cpp main.cpp
+//
+//	Functii WorldDrawer3d:
+//	-init e apelat o singura data, la inceput.
+//	-idle e apelat per frame
+//	-onKey la apasarea unei taste.
+//
+//	Obiecte:
+//	- un obiect este reprezentat prin punct si topologia punctelor (cum sunt legate pctele)
+//	- obiectele sunt compuse din triunghiuri! de exemplu cu 4 puncte si 6 indici de topologie
+//	pot crea 2 triunghiuri adiacente ce impreuna formeaza un dreptunghi.
+//
+//	Sisteme de coordonate:
+//	- sunt 2 tipuri de sisteme de coordonate (fix - bleu&magenta) si dinamic(rosu&albastru)
+//	- ca un obiect sa fie desenat trebuie sa fie atasat unui sistem de coordonate
+//	- cand un obiect e atasat la un sistem de coordonate urmeaza transformarile sistemului.
+//
+//	Control obiecte:
+//	- daca translatez/rotesc un obiect/punct direct (ex: o->translate(1,1)) o fac in coordonate globale
+//	- daca translatez/rotesc un obiect printr-un sistem de coordonate o fac in coordonate locale
+//	- pentru simplitate toate coordonatele mentinute in obiecte(de c++) sunt globale.
+//
+//	Happy coding.
+//----------------------------------------------------------------------------------------------
+
+#include "WorldDrawer3d.h"
+bool WorldDrawer3d::animation=true;
+
+
+//used global vars
+CoordinateSystem3d *cs1;
+Object3d *o1, *o2, *o3, *o4,*o5, *o6;
+
+
+//add
+void WorldDrawer3d::init(){
+	//creeaza 2 sistem de coordonate client
+	cs1 = new CoordinateSystem3d();
+	cs_used.push_back(cs1);
+	
+	//cub
+	std::vector<Point3d> points;
+	std::vector<int> topology;
+	points.push_back(Point3d(1,1,1));
+	points.push_back(Point3d(1,1,-1));
+	points.push_back(Point3d(-1,1,-1));
+	points.push_back(Point3d(-1,1,1));
+	points.push_back(Point3d(1,-1,1));
+	points.push_back(Point3d(1,-1,-1));
+	points.push_back(Point3d(-1,-1,-1));
+	points.push_back(Point3d(-1,-1,1));
+	topology.push_back(0);topology.push_back(1);topology.push_back(2);	//top
+	topology.push_back(2);topology.push_back(3);topology.push_back(0);
+	topology.push_back(6);topology.push_back(5);topology.push_back(4);	//bottom
+	topology.push_back(7);topology.push_back(4);topology.push_back(6);
+	topology.push_back(2);topology.push_back(3);topology.push_back(6);	//left
+	topology.push_back(7);topology.push_back(3);topology.push_back(6);
+	topology.push_back(0);topology.push_back(1);topology.push_back(5);	//right
+	topology.push_back(0);topology.push_back(5);topology.push_back(4);
+	topology.push_back(0);topology.push_back(3);topology.push_back(4);	//front
+	topology.push_back(7);topology.push_back(3);topology.push_back(4);
+	topology.push_back(5);topology.push_back(1);topology.push_back(2);	//back
+	topology.push_back(6);topology.push_back(2);topology.push_back(5);
+	o1 = new Object3d(points,topology);
+	o1->setcolor(1,0.8f,0.9f);
+	//cs1
+	cs1->objectAdd(o1);
+	cs1->translate(10,-3,-10);
+	cs1->rotateXSelf(3.1416f);
+
+	//o2
+	o2 = new Object3d(points,topology);
+	o2->setcolor(1,1,1);
+	cs_basis.objectAdd(o2);
+	cs_basis.objectTranslate(o2,10,0,0);
+
+	//o3
+	o3 = new Object3d(points,topology);
+	o3->setcolor(0,1,0);
+	cs_basis.objectAdd(o3);
+	cs_basis.objectTranslate(o3,0,10,0);
+
+	//o4
+	o4 = new Object3d(points,topology);
+	o4->setcolor(1,1,0);
+	cs_basis.objectAdd(o4);
+	cs_basis.objectTranslate(o4,0,0,10);
+
+	//o5
+	o5 = new Object3d(points,topology);
+	o5->setcolor(1,1,1);
+	cs_basis.objectAdd(o5);
+
+	//o6
+	o6 = new Object3d(points,topology);
+	o6->setcolor(0.5,0.5,0.5);
+	cs_basis.objectAdd(o6);
+	cs_basis.objectTranslate(o6,0,15,0);
+}
+void WorldDrawer3d::onIdle(){	//per frame
+	static int iteration=1;
+	if(animation){
+
+
+		iteration++;
+	}
+}
+
+void WorldDrawer3d::onKey(unsigned char key){
+	switch(key){
+		case KEY_UP:
+			break;
+		case KEY_DOWN:
+			break;
+		case KEY_LEFT:
+			break;
+		case KEY_RIGHT:
+			break;
+		case KEY_SPACE:
+				animation=!animation;
+			break;
+		default:
+			break;
+	}
+}
+
+
+int main(int argc, char** argv){
+	WorldDrawer3d wd3d(argc,argv,600,600,200,100,std::string("Lab 2"));
+	wd3d.init();
+	wd3d.run();
+	return 0;
+}
